121e8dfc1121660d87be7f0f4aeb9cbb394f2a47
pranavkapoor001 <pranavkapoor001@gmail.com>
2019-09-25 21:50:48 +0530

    X00T: Nuke Init

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -243,10 +243,6 @@ BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 # Use Snapdragon LLVM, if available
 TARGET_USE_SDCLANG := true
 
-# Vendor init
-TARGET_INIT_VENDOR_LIB := libinit_X00T
-TARGET_RECOVERY_DEVICE_MODULES := libinit_X00T
-
 # Wifi
 BOARD_HAS_QCOM_WLAN := true
 BOARD_HAS_QCOM_WLAN_SDK := true
